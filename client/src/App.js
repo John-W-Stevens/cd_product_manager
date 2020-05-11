1b2435057cf99273088165aa501287b714cb8624
John Stevens <john.william.stevens1@gmail.com>
2020-05-11 14:37:19 -0700

    Initial commit, achieved MVP for product_manager_I. Which means:
    - Set up MongoDB
    - Set up Express and used Mongoose to tie MongoDB with Express
    - Set up client-side interface with React
    - Linked client and server together with CORS and axios
    
    - So far, client side is only set up to create new products

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./bootstrap.css"
+import CreateProductForm from "./components/CreateProductForm"
+
+function App() {
+  return (
+    <div className="App">
+      <CreateProductForm />
+    </div>
+  );
+}
+
+export default App;
